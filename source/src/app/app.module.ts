7f1f2d013837f586f7db0c6dce16bf2b13094839
onlythenaive <ilya.gubarev@gmail.com>
2018-02-16 08:03:15 +0300

    Main layout component added

--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AppSharedModule} from './common/app-common.module';
+import {AppLayoutComponent} from './layout/app-layout.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AppLayoutComponent
     ],
     imports: [
         AppSharedModule
